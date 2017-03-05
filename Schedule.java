bc12cc29cc24528dc48eed1ad8e35c27c770e876
marper98 <martinperez98@gmail.com>
2017-03-04 22:08:01 -0800

    Changes to Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -3,15 +3,13 @@ import java.util.ArrayList;
 public class Schedule{
   private int beginday;
   private int endday;
-  private boolean[] timeleftTwoDays;
-  private boolean[] timeleftThreeDays;
+  private boolean[][] timeUsed;
   private ArrayList<Class> planned;
 
   public Schedule(int begin, int end){
     beginday = begin;
-    endday = endday;
-    timeleftTwoDays = new boolean[beginday-endday];
-    timeleftThreeDays = new boolean[beginday-endday];
+    endday = end;
+    timeUsed = new boolean[5][(end-begin)*2];
     planned = new ArrayList();
   }
 
@@ -21,24 +19,32 @@ public class Schedule{
   public int getEndDay(){
     return endday;
   }
-  public boolean getTimeleft(int time, boolean twodays){
-    if(twodays){
-      return timeleftTwoDays[time];
-    }
-    return timeleftThreeDays[time];
-  }
-  private void setTimeLeft(int time, boolean twodays){
-    if(twodays){
-      timeleftTwoDays[time] = true;
-    }else{
-      timeleftThreeDays[time] = true;
-    }
-  }
-  public void planNewClass(Class newClass, int time, boolean twodays){
-    planned.add(newClass);
-    setTimeLeft(time, twodays);
-  }
   public ArrayList<Class> getPlan(){
     return planned;
+  }
+  public boolean planCourse(Class planned){
+    boolean success = true;
+    if(planned.getBegin() < beginday || planned.getBegin() > endday){
+      success = false;
+      return success;
+    }
+    if(planned.getEnd() < beginday || planned.getEnd() > endday){
+      success = false;
+      return success;
+    }
+    int startOfClass = (int)((planned.getBegin() - beginday)*2);
+    int endOfClass = (int)((planned.getEnd() - beginday)*2);
+    for(int i=0; i<timeUsed.length; i++){
+      if(planned.getDays()[i]){
+        for(int k=startOfClass; k<endOfClass; k++){
+          if(timeUsed[i][k]){
+            success = false;
+            return success;
+          }
+        } 
+      }
+    }
+    this.planned.add(planned);
+    return success;
   }
 }
